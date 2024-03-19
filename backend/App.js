ebb88bfba2ebd22a660772915fb939195b58a0a5
SpiderWeb-code <125570323+SpiderWeb-code@users.noreply.github.com>
2024-03-19 15:46:26 +0100

    Légére modification

--- backend/App.js
+++ backend/App.js
@@ -7,27 +7,22 @@ const path = require('path');
 require('dotenv').config();
 const rateLimit = require('express-rate-limit');
 
-// connexion à ma base de données
-//`mongodb+srv://leiroz26:${process.env.MDP_BDD}@bddmonvieuxgrimoire.rnsqoxc.mongodb.net/?retryWrites=true&w=majority`
 mongoose.connect(`mongodb+srv://SharkO:qWPipuED7pcujQyD@cluster0.n7cz2me.mongodb.net/données-serveur?retryWrites=true&w=majority&appName=Cluster0`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-// fonction pour limiter le nombre de requetes à 10 sur 1 min
 const limiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 30, // 20 requêtes maximum par minute
+  windowMs: 60 * 1000,
+  max: 30,
   message: 'Trop de requêtes effectuées. Réessayer plus tard.'
 });
 
 app.use(limiter);
 
-// Nous mets à disposition le contenu de toutes les requetes qui contiennet du JSON
 app.use(express.json());
 
-// Gestion des CORS pour toutes les routes
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
